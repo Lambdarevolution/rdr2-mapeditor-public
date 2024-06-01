9710e66ec4a088392acb8d371583b0de64341dab
Lambdarevolution <67820669+Lambdarevolution@users.noreply.github.com>
2024-06-01 17:14:43 +0200

    Create Prompts.h
    New Prompts.h

---
+++ rewrite/Prompts/Prompts.h
@@ -0,0 +1,64 @@
+/*
+| License: Unlicense
+| Created by: Lambdarevolution(at)gmail.com / 2020-03-28
+| Purpose: Create and manages rdr2 prompts easily using a singleton class.
+| Modified for public release 2024-05-31
+| Uses the sdk native header. Alot of the natives in the sdk header are unnamed so
+| you will see alot native hashes. 
+| Note that i havent had time to test/add all modes and natives!
+*/
+#ifndef RDR2_MAPEDITOR_PROMPTS_H_
+#define RDR2_MAPEDITOR_PROMPTS_H_
+
+#include <vector>
+#include <string>
+
+#include "..\..\inc\natives.h"
+#include "..\..\inc\types.h"
+
+// This is a pretty ugly fix of a holder struct but meh it works.
+struct sPrompt {
+  int id;
+  std::string tag;
+};
+
+class Prompts {
+ private:
+  int m_horizontalid;
+  static Prompts* m_instance;
+  std::vector<sPrompt*> Promptlist;
+
+ public:
+  enum EMode {
+    indefinitely_mode,
+    hold_mode,
+    hold_mode_running,
+    mash_mode,
+    mash_mode_indefinitely_mode,
+    pressed_timed_mode,
+    standard_mode
+  };
+
+  static Prompts* getInstance();
+  void CreatePrompt(char* t_input, char* t_text, EMode t_mode, bool t_enabled, bool t_visible, char* t_tag, int t_timer, int t_mashes);
+  void DeletePrompt(char* t_tag);
+  void HidePrompt(char* t_tag);
+  void ShowPrompt(char* t_tag);
+  void DisablePrompt(char* t_tag);
+  void EnablePrompt(char* t_tag);
+  void SetPromptAttribute(char* t_tag, int t_attribute, bool t_enable); 
+  void SetPromptText(char* t_tag, const char* t_txt);
+  void SetPriority(char* t_tag, int t_priority);
+  float GetPromptProgress(char* t_tag);  
+  void SetUrgentPulsing(char* t_tag, bool t_state);  
+  bool IsUrgentPulsingEnabled(char* t_tag);  
+  bool DoPromptExist(char* t_tag);
+  bool IsPromptValid(char* t_tag);
+  bool IsPromptBeingPressed(char* t_tag);
+  bool IsPromptEnabled(char* t_tag);
+  bool IsPromptCompleted(char* t_tag, EMode mode);
+  void SetHorizontalOrientation();
+  void ResetHorizontalOrientation();
+  void DeleteAllEditorPrompts();
+};
+#endif  // RDR2_MAPEDITOR_PROMPTS_H_
