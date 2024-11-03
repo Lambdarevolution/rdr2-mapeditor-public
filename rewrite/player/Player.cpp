e8ed4080a807a582961176a789112b8b5bf2d98c
Lambdarevolution <67820669+Lambdarevolution@users.noreply.github.com>
2024-11-03 08:24:11 +0100

    Update and rename player.cpp to Player.cpp
    Update code style

--- rewrite/player/Player.cpp
+++ rewrite/player/Player.cpp
@@ -5,25 +5,26 @@
 PlayerInfo::PlayerInfo() {
   if (PED::IS_PED_ON_MOUNT(PLAYER::GET_PLAYER_PED(PLAYER::PLAYER_ID()))) {
     Hash lWeaponHash;
-    this->m_on_mount = true;
-    this->m_mount_ped = PED::GET_MOUNT(PLAYER::PLAYER_PED_ID());
-    this->m_mount_position = ENTITY::GET_ENTITY_COORDS(this->m_mount_ped, false, true);
-    this->m_player_position = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), false, true);
+    this->mOnMount = true;
+    this->mMountPed = PED::GET_MOUNT(PLAYER::PLAYER_PED_ID());
+    this->mMountPosition = ENTITY::GET_ENTITY_COORDS(this->mMountPed, false, true);
+    this->mPlayerPosition = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), false, true);
     // Weapon.
     WEAPON::GET_CURRENT_PED_WEAPON(PLAYER::GET_PLAYER_PED(PLAYER::PLAYER_ID()), &lWeaponHash, 0, 0, 0);
-    this->m_weapon = lWeaponHash;
+    this->mWeapon = lWeaponHash;
 
     // Remove ped from mount.
     // Not removing the ped can cause issues its safer to remount on exit.
     PED::_0x5337B721C51883A9(PLAYER::GET_PLAYER_PED(PLAYER::PLAYER_ID()), 1, 1);
 
-  } else {
+  }
+  else {
     Hash lWeaponHash;
-    this->m_on_mount = false;
-    this->m_player_position = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), false, true);
+    this->mOnMount = false;
+    this->mPlayerPosition = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), false, true);
     // Weapon.
     WEAPON::GET_CURRENT_PED_WEAPON(PLAYER::PLAYER_PED_ID(), &lWeaponHash, 0, 0, 0);
-    this->m_weapon = lWeaponHash;
+    this->mWeapon = lWeaponHash;
   }
 }
 
@@ -34,9 +35,9 @@ PlayerInfo::~PlayerInfo() {
 void PlayerInfo::ResetPlayer() const {
   ENTITY::SET_ENTITY_COORDS(
    PLAYER::PLAYER_PED_ID(),
-   m_player_position.x,
-   m_player_position.y,
-   (m_player_position.z + 0.05f) - (PED::_0x1D491CCF7211FB74(PLAYER::PLAYER_PED_ID()) / 2),
+   mPlayerPosition.x,
+    mPlayerPosition.y,
+   (mPlayerPosition.z + 0.05f) - (PED::_0x1D491CCF7211FB74(PLAYER::PLAYER_PED_ID()) / 2),
    0,
    0,
    1,
@@ -45,28 +46,28 @@ void PlayerInfo::ResetPlayer() const {
 
   WEAPON::SET_CURRENT_PED_WEAPON( 
    PLAYER::PLAYER_PED_ID(),
-   m_weapon,
+   mWeapon,
    1,
    0,
    0,
    0
   );
 
-  if (m_on_mount) {
+  if (mOnMount) {
   ENTITY::SET_ENTITY_COORDS(
-   m_mount_ped,
-   m_mount_position.x,
-   m_mount_position.y,
-   m_mount_position.z,
+   mMountPed,
+   mMountPosition.x,
+   mMountPosition.y,
+   mMountPosition.z,
    0,
    0,
    1,
    false
   );
 
-   ENTITY::SET_ENTITY_COLLISION(m_mount_ped, true, true);
-   ENTITY::SET_ENTITY_VISIBLE(m_mount_ped, true);
-   PED::_0x028F76B6E78246EB(PLAYER::PLAYER_PED_ID(), m_mount_ped, -1, 0);
+   ENTITY::SET_ENTITY_COLLISION(mMountPed, true, true);
+   ENTITY::SET_ENTITY_VISIBLE(mMountPed, true);
+   PED::_0x028F76B6E78246EB(PLAYER::PLAYER_PED_ID(), mMountPed, -1, 0);
   }
 
   PLAYER::SET_EVERYONE_IGNORE_PLAYER(PLAYER::PLAYER_ID(), false);
