925e79622b36fa14115def63ecc847d2da8cb584
Lambdarevolution <67820669+Lambdarevolution@users.noreply.github.com>
2025-05-20 21:43:25 +0200

    Update EditorCamera.h
    A bit more modern, and fixed some consistency issues.

--- rewrite/Camera/EditorCamera.h
+++ rewrite/Camera/EditorCamera.h
@@ -1,6 +1,7 @@
 /*
 | License: The Unlicense
-| Created by: Lambdarevolution(at)gmail.com / 2020-04-01 - rewritten 2024-11-10
+| Created by: Lambdarevolution(at)gmail.com 
+| Rewritten: 2025
 */
 #ifndef RDR2_MAPEDITOR_CAMERA_H
 #define RDR2_MAPEDITOR_CAMERA_H
@@ -12,29 +13,29 @@ class MapEditorCamera {
   MapEditorCamera();
   ~MapEditorCamera();
   Camera GetCamera() const;
-  void SetCameraSpeed(const float speed);
-  void SetCameraFov(const float fov);
-  void SetCameraRaiseButton(char* input);
-  void SetCameraLowerButton(char* input);
+  void SetCameraSpeed(const float t_speed);
+  void SetCameraFov(const float t_fov);
+  void SetCameraRaiseButton(char* t_input);
+  void SetCameraLowerButton(char* t_input);
   float GetCameraSpeed() const;
   float GetCameraFov() const;
   void CreateCamera();
-  void UpdateCamera(const bool blockinput);
+  void UpdateCamera(const bool t_blockinput);
   void ActivateCamera() const;
   void DeactivateCamera() const;
-  void SetLetterBox(const bool state);
+  void SetLetterBox(const bool t_state);
   bool HasLetterBoxEffect() const;
   Vector3 GetCameraRightVector() const;
   Vector3 GetCameraForwardVector() const;
-  inline float Clamp(float value, const float min, const float max);
+  inline float Clamp(float t_value, const float t_min, const float t_max);
 
  private:
-  bool mLetterBox;
-  Camera mCamera;
-  char* mRaiseCameraInput;
-  char* mLowerCameraInput;
-  float mBoostValue;
-  float mCameraSpeed;
-  float mFov;
+   bool m_LetterBox{ false };
+   Camera m_Camera{ NULL };
+   char* m_RaiseCameraInput{ nullptr };
+   char* m_LowerCameraInput{ nullptr };
+   float m_BoostValue{ 1.5f };
+   float m_CameraSpeed{ 1.0f };
+   float m_Fov{ 50.0f };
 };
 #endif  // RDR2_MAPEDITOR_CAMERA_H
