72e77c3d6da796190ff6a8cb14345d96b65e1b8a
Lambdarevolution <67820669+Lambdarevolution@users.noreply.github.com>
2025-06-08 19:34:28 +0200

    Update TimeMenu.h

--- rewrite/menu/TimeMenu.h
+++ rewrite/menu/TimeMenu.h
@@ -1,21 +1,25 @@
+/*
+| License: Unlicense
+| Created by: Lambdarevolution(at)gmail.com
+*/
+
 #ifndef RDR2_MAPEDITOR_TIME_MENU_H
 #define RDR2_MAPEDITOR_TIME_MENU_H
 
 class IntMenuItem;
 class MenuButton;
 class MenuCheckBox;
-class MenuStringItem;
 
 namespace TimeMenu {
-  void HandleHourForward(MenuButton* button);
-  void HandleHourBackwards(MenuButton* button);
-  void HandleClockPaused(MenuCheckBox* item);
-  void HandleYearRight(IntMenuItem* item);
-  void HandleYearLeft(IntMenuItem* item);
-  void HandleMonthRight(IntMenuItem* item);
-  void HandleMonthLeft(IntMenuItem* item);
-  void HandleDayRight(IntMenuItem* item);
-  void HandleDayLeft(IntMenuItem* item);
-  void CreateTimeListMenu(MenuButton* button);
+  void HandleHourForward(MenuButton* t_item);
+  void HandleHourBackwards(MenuButton* t_item);
+  void HandleClockPaused(MenuCheckBox* t_item);
+  void HandleYearRight(IntMenuItem* t_item);
+  void HandleYearLeft(IntMenuItem* t_item);
+  void HandleMonthRight(IntMenuItem* t_item);
+  void HandleMonthLeft(IntMenuItem* t_item);
+  void HandleDayRight(IntMenuItem* t_item);
+  void HandleDayLeft(IntMenuItem* t_item);
+  void CreateTimeMenu(MenuButton* t_item);
 }
 #endif
